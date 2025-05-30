454220b5342716d2116fdb90b7f00a4be010e1db
Jolinna Mae Tibulan <jolinna.mae.tibulan@accenture.com>
2025-05-30 07:34:04 +0000

    CSS intialized to pages

--- webapp/controller/Page1.controller.js
+++ webapp/controller/Page1.controller.js
@@ -9,27 +9,39 @@ sap.ui.define([
         // Navigate to Home (Page1)
         onHomePress: function () {
             var oBundle = this.getView().getModel("i18n").getResourceBundle();  // Get i18n model
-            var homeMessage = oBundle.getText("homeMessage");  // Fetch the home message from i18n properties
-
-            MessageBox.show(homeMessage, {
-                onClose: function () {
+            var homeMessage = oBundle.getText("homeMessage");  // Fetch the previous page message from i18n properties
+        
+            // Show MessageBox
+            MessageBox.show(
+                homeMessage,  // Message content
+                MessageBox.Icon.INFORMATION,  // MessageBox type (e.g., Information)
+                oBundle.getText("pageTitle"),  // MessageBox title from i18n
+                [sap.m.MessageBox.Action.OK],  // Buttons for the MessageBox
+                // onClose function to navigate to Page1
+                function() {
                     var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                     oRouter.navTo("RoutePage1");  // Navigate to Page1 after the MessageBox is closed
-                }.bind(this)
-            });
+                }.bind(this)  // Binding the context to the controller
+            );
         },
 
-        // Navigate to Next Page (Page2)
+        // Navigate to Previous Page (Page1)
         onNavigateToPage2: function () {
             var oBundle = this.getView().getModel("i18n").getResourceBundle();  // Get i18n model
-            var nextPageMessage = oBundle.getText("nextPageMessage");  // Fetch the next page message from i18n properties
-
-            MessageBox.show(nextPageMessage, {
-                onClose: function () {
+            var nextPageMessage = oBundle.getText("nextPageMessage");  // Fetch the previous page message from i18n properties
+        
+            // Show MessageBox
+            MessageBox.show(
+                nextPageMessage,  // Message content
+                MessageBox.Icon.INFORMATION,  // MessageBox type (e.g., Information)
+                oBundle.getText("pageTitle"),  // MessageBox title from i18n
+                [sap.m.MessageBox.Action.OK],  // Buttons for the MessageBox
+                // onClose function to navigate to Page2
+                function() {
                     var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                     oRouter.navTo("RoutePage2");  // Navigate to Page2 after the MessageBox is closed
-                }.bind(this)
-            });
+                }.bind(this)  // Binding the context to the controller
+            );
         }
     });
 });
--- webapp/controller/Page2.controller.js
+++ webapp/controller/Page2.controller.js
@@ -9,27 +9,40 @@ sap.ui.define([
         // Navigate to Home (Page1)
         onHomePress: function () {
             var oBundle = this.getView().getModel("i18n").getResourceBundle();  // Get i18n model
-            var homeMessage = oBundle.getText("homeMessage");  // Fetch the home message from i18n properties
-
-            MessageBox.show(homeMessage, {
-                onClose: function () {
+            var homeMessage = oBundle.getText("homeMessage");  // Fetch the previous page message from i18n properties
+        
+            // Show MessageBox
+            MessageBox.show(
+                homeMessage,  // Message content
+                MessageBox.Icon.INFORMATION,  // MessageBox type (e.g., Information)
+                oBundle.getText("pageTitle"),  // MessageBox title from i18n
+                [sap.m.MessageBox.Action.OK],  // Buttons for the MessageBox
+                // onClose function to navigate to Page1
+                function() {
                     var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                     oRouter.navTo("RoutePage1");  // Navigate to Page1 after the MessageBox is closed
-                }.bind(this)
-            });
+                }.bind(this)  // Binding the context to the controller
+            );
         },
 
         // Navigate to Previous Page (Page1)
         onNavigateToPage1: function () {
             var oBundle = this.getView().getModel("i18n").getResourceBundle();  // Get i18n model
             var prevPageMessage = oBundle.getText("previousPageMessage");  // Fetch the previous page message from i18n properties
-
-            MessageBox.show(prevPageMessage, {
-                onClose: function () {
+        
+            // Show MessageBox
+            MessageBox.show(
+                prevPageMessage,  // Message content
+                MessageBox.Icon.INFORMATION,  // MessageBox type (e.g., Information)
+                oBundle.getText("pageTitle"),  // MessageBox title from i18n
+                [sap.m.MessageBox.Action.OK],  // Buttons for the MessageBox
+                // onClose function to navigate to Page1
+                function() {
                     var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                     oRouter.navTo("RoutePage1");  // Navigate to Page1 after the MessageBox is closed
-                }.bind(this)
-            });
+                }.bind(this)  // Binding the context to the controller
+            );
         }
+        
     });
 });
